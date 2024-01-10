46cc2d5f308b2e4ee5aec7d480e264603264f94e
Brian Maher <bmaher91@gmail.com>
2024-01-10 16:41:53 -0700

    Stub HealthKit plugin

---
+++ plugins/healthkit/README.md
@@ -0,0 +1,3 @@
+# Godot iOS HealthKit plugin
+
+## Methods
---
+++ plugins/healthkit/healthkit_module.cpp
@@ -0,0 +1,54 @@
+/*************************************************************************/
+/*  healthkit_module.cpp                                                 */
+/*************************************************************************/
+/*                       This file is part of:                            */
+/*                           GODOT ENGINE                                */
+/*                      https://godotengine.org                          */
+/*************************************************************************/
+/* Copyright (c) 2007-2021 Juan Linietsky, Ariel Manzur.                 */
+/* Copyright (c) 2014-2021 Godot Engine contributors (cf. AUTHORS.md).   */
+/*                                                                       */
+/* Permission is hereby granted, free of charge, to any person obtaining */
+/* a copy of this software and associated documentation files (the        */
+/* "Software"), to deal in the Software without restriction, including   */
+/* without limitation the rights to use, copy, modify, merge, publish,   */
+/* distribute, sublicense, and/or sell copies of the Software, and to    */
+/* permit persons to whom the Software is furnished to do so, subject to */
+/* the following conditions:                                             */
+/*                                                                       */
+/* The above copyright notice and this permission notice shall be        */
+/* included in all copies or substantial portions of the Software.       */
+/*                                                                       */
+/* THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,       */
+/* EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF    */
+/* MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.*/
+/* IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY  */
+/* CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT,  */
+/* TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE     */
+/* SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.                */
+/*************************************************************************/
+
+#include "healthkit_module.h"
+
+#include "core/version.h"
+
+#if VERSION_MAJOR == 4
+#include "core/config/engine.h"
+#else
+#include "core/engine.h"
+#endif
+
+#include "healthkit.h"
+
+HealthKit *healthKit;
+
+void register_healthkit_types() {
+	healthKit = memnew(HealthKit);
+	Engine::get_singleton()->add_singleton(Engine::Singleton("HealthKit", healthKit));
+}
+
+void unregister_healthkit_types() {
+	if (healthKit) {
+		memdelete(healthKit);
+	}
+}
