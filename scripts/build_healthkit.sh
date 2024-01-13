d9512f89ad3e641dbea7ff1918ef130f6cb19ac6
Brian Maher <bmaher91@gmail.com>
2024-01-13 13:47:23 -0700

    Update build script to not run additional steps if prior steps fail

--- scripts/build_healthkit.sh
+++ scripts/build_healthkit.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
-rm -rf ./bin/.*healthkit.* ./healthkit/.*.d ./healthkit/.*.o
-scons target=release_debug arch=arm64 simulator=no plugin=healthkit version=4.0
-./scripts/generate_static_library.sh healthkit release_debug 4.0
-./scripts/generate_xcframework.sh healthkit release_debug 4.0
-mv ./bin/healthkit.release_debug.xcframework ./bin/healthkit.xcframework
+
+rm -rf ./bin/.*healthkit.* ./healthkit/.*.d ./healthkit/.*.o && 
+    scons target=release_debug arch=arm64 simulator=no plugin=healthkit version=4.0 && 
+    ./scripts/generate_static_library.sh healthkit release_debug 4.0 &&
+    ./scripts/generate_xcframework.sh healthkit release_debug 4.0 &&
+    mv ./bin/healthkit.release_debug.xcframework ./bin/healthkit.xcframework
