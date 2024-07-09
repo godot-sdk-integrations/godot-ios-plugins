0914376c1eca60ba0a4416ae59efead12cecf862
lucas <452252+catabriga@users.noreply.github.com>
2024-07-09 22:22:07 +0200

    add AlbumIOS skeleton

---
+++ plugins/album/README.md
@@ -0,0 +1,1 @@
+# Godot iOS Album plugin
---
+++ plugins/album/album_ios_module.cpp
@@ -0,0 +1,54 @@
+/*************************************************************************/
+/*  album_ios_module.cpp                                                 */
+/*************************************************************************/
+/*                       This file is part of:                           */
+/*                           GODOT ENGINE                                */
+/*                      https://godotengine.org                          */
+/*************************************************************************/
+/* Copyright (c) 2007-2021 Juan Linietsky, Ariel Manzur.                 */
+/* Copyright (c) 2014-2021 Godot Engine contributors (cf. AUTHORS.md).   */
+/*                                                                       */
+/* Permission is hereby granted, free of charge, to any person obtaining */
+/* a copy of this software and associated documentation files (the       */
+/* "Software"), to deal in the Software without restriction, including   */
+/* without limitation the rights to use, copy, modify, merge, publish,   */
+/* distribute, sublicense, and/or sell copies of the Software, and to    */
+/* permit persons to whom the Software is furnished to do so, subject to */
+/* the following conditions:                                             */
+/*                                                                       */
+/* The above copyright notice and this permission notice shall be        */
+/* included in all copies or substantial portions of the Software.       */
+/*                                                                       */
+/* THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,       */
+/* EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF    */
+/* MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.*/
+/* IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY  */
+/* CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT,  */
+/* TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE     */
+/* SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.                */
+/*************************************************************************/
+
+#include "album_ios_module.h"
+
+#include "core/version.h"
+
+#if VERSION_MAJOR == 4
+#include "core/config/engine.h"
+#else
+#include "core/engine.h"
+#endif
+
+#include "album_ios.h"
+
+AlbumIOS *album_ios;
+
+void register_album_ios_types() {
+	album_ios = memnew(AlbumIOS);
+	Engine::get_singleton()->add_singleton(Engine::Singleton("AlbumIOS", album_ios));
+}
+
+void unregister_album_ios_types() {
+	if (album_ios) {
+		memdelete(album_ios);
+	}
+}
